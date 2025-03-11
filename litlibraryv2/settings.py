cc750d2bee3204c313967d99c97c0a72bc22a460
vmr093 <ranjha.viola@gmail.com>
2025-03-10 22:01:48 -0500

    Configured DATABASE_URL for Neon PostgreSQL

--- litlibraryv2/settings.py
+++ litlibraryv2/settings.py
@@ -70,7 +70,7 @@ DATABASES = {
     'default': dj_database_url.config(
         default=os.getenv('DATABASE_URL'),
         conn_max_age=600,
-        ssl_require=True  # Ensure SSL for secure connection on Heroku
+        ssl_require=True  # Ensure SSL is required for secure connections
     )
 }
 # Password validation
